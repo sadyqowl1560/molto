fb465adfd815661f61dde702a4b4bae661ed3525
sadyqowl1560 <sadyqowl1560@gmail.com>
2020-12-29 21:09:11 +0200

    Lockscreen : pushing deompiled source after fixing
    Signed-off-by: sadyqowl1560 <sadyqowl1560@gmail.com>

---
+++ molto.c
@@ -0,0 +1,31 @@
+// App Link : https://apkpure.com/lock-screen/com.idev.app.security.lockscreen
+// Thanks for phhusson for help to reduce cpu usage
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+void main(void)
+{
+  int run_or_no;
+  int works_or_stopped;
+  while( true ) 
+  {
+    run_or_no = system("pidof com.idev.app.security.lockscreen");
+    works_or_stopped = system("dumpsys activity services | grep com.idev.app.security.lockscreen/idev.lockscreen.control.LockScreenService");
+    if (run_or_no == 0) {
+      if (works_or_stopped != 0) {
+        system("pm enable com.idev.app.security.lockscreen");
+        system("pm enable com.idev.app.security.lockscreen/idev.lockscreen.activity.MainActivity");
+        system("input keyevent 26");
+        system("lockscreen");
+        system("am startservice com.idev.app.security.lockscreen/idev.lockscreen.activity.MainActivity");
+      }
+    }
+    else {
+      system("pm enable com.idev.app.security.lockscreen");
+      system("input keyevent 26");
+      system("lockscreen");
+    }
+    sleep(10);
+  } 
+}
+
